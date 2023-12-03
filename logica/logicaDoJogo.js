a7d7914ef6689d6414e1eb4d7b84072a1387bec1
João Srbek <joao.srbek@estudante.ufjf.br>
2023-12-03 20:21:06 -0300

    Cria função para girar os dados e começa a arrumar a logica do jogo

---
+++ logica/logicaDoJogo.js
@@ -0,0 +1,5 @@
+function giraDado(){
+    return Math.floor(Math.random()*6);
+}
+
+export{giraDado}
---
+++ logica/main.js
@@ -0,0 +1,4 @@
+import{giraDado} from"./logicaDoJogo.js"
+
+let dadoDoBot=giraDado()
+console.log(`${randomCasaBot}`)
