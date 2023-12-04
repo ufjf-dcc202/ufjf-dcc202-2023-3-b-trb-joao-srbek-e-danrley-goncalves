7d5b89812d79219c9553419d40642ffde107324b
DanrleyGD <danrley.dias@estudante.ufjf.br>
2023-12-04 16:05:00 -0300

    Co-authored-by: JoaoSrbek <JoaoSrbek@users.noreply.github.com> cria funcões selecionaCelula IniciaJogo e objeto player

--- logica/logicaDoJogo.js
+++ logica/logicaDoJogo.js
@@ -1,16 +1,50 @@
-let jogadorAtual='user';
-function giraDado(){
-    return Math.floor(Math.random()*6)+1;
-}
-function trocaQuemJoga(jogadorAtual){
-    if(jogadorAtual==user){
-        return jogadorAtual;
-        jogadorAtual='bot';
+document.addEventListener("DOMContentLoaded", function () {
+  var celTabela = document.querySelector("td");
+  celTabela.forEach(function (celTabela) {
+    celTabela.addEventListener("click", function () {
+      selecionaCelula(celTabela);
+    });
+  });
+});
+
+const btn = document.querySelector('btnIniciar');
+btn.addEventListener('click', function(){
+    
+});
+
+let player = {
+  nome: "user",
+  operacao1: function giraDado() {
+    return Math.floor(Math.random() * 6) + 1;
+  },
+  operacao2: function trocaQuemJoga(nome) {
+    if (nome == user) {
+      nome = "bot";
+    } else {
+      nome = "user";
+    }
+  },
+  operacao3: function jogadaPlayerAtual(){
+    if(nome==bot){
+        let dado = this.giraDado();
+        let table = document.getElementById('tabelaBot');
+        let tds = table.getElementsByTagName('td');
+        let randomTd = tds[Math.floor(Math.random() * tds.length)];
+        randomTd.innerHTML = dado;
     }else{
-        return jogadorAtual;
-        jogadorAtual='user';
+        
+    }
+  }  
+};
+
+function selecionaCelula() {
+    celTabela = giraDado();
+}
+
+function iniciaJogo(){
+    if (btnIniciar) {
+        
     }
 }
 
-
-export{giraDado,jogadaBot, trocaQuemJoga,adicionaNaTabelaDoBot}
+export { giraDado, trocaQuemJoga, jogadaPlayerAtual};
