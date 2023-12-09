e6c4a159965d452ceeb2e10f6b9bbc57e208abd4
DanrleyGD <danrley.dias@estudante.ufjf.br>
2023-12-09 19:08:13 -0300

    inicia criação do modo para click nas celulas da tabela usuario

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
-import{giraDado,getDado, trocaTurno} from "./logicaDoJogo.js";
+import{giraDado,getDado, jogada} from "./logicaDoJogo.js";
 
 /*const botaoIniciar = document.querySelector("#btnIniciar");
 botaoIniciar.addEventListener("click", giraDado());*/
-
+let turno = true;
+const jogadaRola =jogada(dado);
 const dado = getDado();
+const celulas = document.querySelectorAll('.espacoUser');
+
 document.querySelector("#dado").textContent = dado;
+
+
+celulas.forEach(celula => {
+  celula.addEventListener('click', function() {
+    if (celula.textContent === 0) {
+       celula.textContent = giraDado(); 
+    }
+    else{
+        return;
+    }
+  });
+});
 
 console.log(`${dado}`);
 
